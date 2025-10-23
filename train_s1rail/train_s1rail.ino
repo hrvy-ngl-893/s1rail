b812e98944638164741baff36818d49d2abad03c
Harvy Angelo Tan <harvy_angelo.tan983@icloud.com>
2025-10-23 23:40:37 +0800

    Added MPU6050, prone to MPU6050 setup errors

--- train_s1rail/train_s1rail.ino
+++ train_s1rail/train_s1rail.ino
@@ -1,3 +1,7 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
 #define IR_PIN 33
 #define MOTOR_PIN 25
 
@@ -13,11 +17,27 @@ bool lastStableState = LOW;
 unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 50; // must stay stable for 50ms
 
+// ======= MPU6050 Variables =======
+Adafruit_MPU6050 mpu;
+unsigned long lastMPUPrint = 0;
+const unsigned long mpuInterval = 1000; // print every 1s
+
 void setup() {
   Serial.begin(115200);
   pinMode(IR_PIN, INPUT_PULLUP);
   pinMode(MOTOR_PIN, OUTPUT);
   digitalWrite(MOTOR_PIN, HIGH); // default running (black)
+
+  // ======= MPU6050 Initialization =======
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip!");
+    while (1) delay(10);
+  }
+  Serial.println("MPU6050 found!");
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  delay(100);
 }
 
 void loop() {
@@ -45,7 +65,7 @@ void loop() {
       digitalWrite(MOTOR_PIN, HIGH);
     } else {
       digitalWrite(MOTOR_PIN, LOW);
-      return;
+      // MPU readings still allowed while paused
     }
   }
 
@@ -69,4 +89,19 @@ void loop() {
   }
 
   lastStableState = stableState;
+
+  // ======= MPU6050 Data Every 1 Second =======
+  if (currentMillis - lastMPUPrint >= mpuInterval) {
+    lastMPUPrint = currentMillis;
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    Serial.println("=== MPU6050 Readings ===");
+    Serial.print("Accel X: "); Serial.print(a.acceleration.x); Serial.print(" m/s^2, ");
+    Serial.print("Y: "); Serial.print(a.acceleration.y); Serial.print(", Z: "); Serial.println(a.acceleration.z);
+    Serial.print("Gyro  X: "); Serial.print(g.gyro.x); Serial.print(" rad/s, ");
+    Serial.print("Y: "); Serial.print(g.gyro.y); Serial.print(", Z: "); Serial.println(g.gyro.z);
+    Serial.print("Temp: "); Serial.print(temp.temperature); Serial.println(" °C");
+    Serial.println("=========================");
+  }
 }
